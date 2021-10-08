7920f8c8b3111635055c58a6c1d732cc0d6d1679
KervinJQP <49247447+KervinJQP@users.noreply.github.com>
2021-10-08 10:12:31 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -124,7 +124,7 @@ def home():
 
 @app.route('/connections')
 def connect():
-    return render_template('connections.html',elements_files=find_files.all_frontend_files())  
+    return render_template('connections.html')  
 
   
 @app.route("/<device>/<action>")
@@ -148,7 +148,7 @@ def action(device, action):
            #servo.value = None
            #sleep(5)
        p.ChangeDutyCycle(40)
-       return render_template('connections.html',elements_files=find_files.all_frontend_files())
+       return render_template('connections.html')
 
 
 @app.route('/',methods=['GET','POST'])
